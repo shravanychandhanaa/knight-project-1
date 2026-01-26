780a93695d73b4144eecef4e7c6e3ed08b6106a2
Shravani Devarakonda <shravanychandhanaa28@gmail.com>
2026-01-27 00:34:19 +0530

    demo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Knight Secured CI/CD pipeline!\n'
+    return 'Hello from Knight Secured CI/CD!\n'
 
 
 if __name__ == '__main__':
